72be8463cd48ca23caa31141decfa55a2e848afe
mayankb789 <cyberpsycho00000@gmail.com>
2025-05-23 20:35:20 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,7 @@
+#program code for finding that the number is odd or Even 
+num=int(input('Enter a number: '))
+if num%2==0:
+ print(num,"is an even number")
+else:
+ print(num,"is an odd number")
+ print("thank you")
---
+++ task2.py
@@ -0,0 +1,5 @@
+#program code for inding the sum of integer no. using for loop.
+sum=0
+for i in range(1,51):
+ sum=sum+i
+print("the summition of 1 to 50 is :",sum)

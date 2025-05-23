829baa580352e168b56c1fc59d91a445c65c58f6
mayankb789 <cyberpsycho00000@gmail.com>
2025-05-23 21:56:30 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,13 @@
+#program for addition subtraction multiplication and division of two integer numbers 
+a=input('Enter first number')
+a=int(a)
+b=input('\nEnter second number')
+b=int(b)
+c=a+b
+print('\naddition',c)
+c=a-b
+print('substraction',c)
+c=a*b
+print('multiplication',c)
+c=a/b
+print('division',c)
---
+++ Taskk 2.py
@@ -0,0 +1,8 @@
+# program for personalized greetings 
+a=input('Enter your first name: ')
+a=str(a)
+b=input('\nEnter your last name: ')
+b=str(b)
+c=a+b
+c=str(c)
+print("\nhello, " + c + "!" +" welcome to the python progra")
